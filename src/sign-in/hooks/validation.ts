8932391e9a1a9220d59e6c9c1370204f6b15f5ea
ryuhojun <rhj080471@gmail.com>
2025-05-27 23:59:20 +0900

    feat: 이메일 및 비밀번호 유효성 검사 테스트 케이스 작성해보기

--- src/sign-in/hooks/validation.ts
+++ src/sign-in/hooks/validation.ts
@@ -1,3 +1,16 @@
 export const sum = (a: number, b: number) => {
   return a + b;
 };
+
+export const validate = (email: string, password: string) => {
+  if (!email || !password) {
+    return {
+      error: "이메일 또는 비밀번호를 입력해주세요.",
+    };
+  }
+
+  return {
+    email,
+    password,
+  };
+};
